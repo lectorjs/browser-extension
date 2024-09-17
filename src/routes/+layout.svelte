ef8d80a7e25e42f9732f44692e74dc029a1651df
ju4n97 <juanmesa2097@gmail.com>
2024-09-17 07:20:25 -0500

    chore: setup unocss & shadcn

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import "@unocss/reset/tailwind.css";
+    import "virtual:uno.css";
+    import "../app.css";
+
+    const { children } = $props();
+</script>
+
+{@render children()}
